241b92d76158a8e9c530b1ae4ae36faa64952d3e
Tanjona <trakoto05@gmail.com>
2023-05-08 10:04:44 +0300

    BaseProject
    Base Project in Laravel

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,31 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    session()->put('iduser',0) ;
+    return view('login');
 });
+
+Route::post('/traitementLogin',[Controller::class,'TraitementLogin'])->name('traitementLogin') ;
+
+Route::get('/accueil.html',[Controller::class,'Accueil']);
+
+Route::post('/insererActu',[Controller::class,'InsererActualite'])->name('insererActu') ;
+
+Route::post('/insererAnalyse',[Controller::class,'InsererAnalyse'])->name('insererAnalyse') ;
+
+Route::get('/listeActu.html',[Controller::class,'ListeActualite']);
+
+Route::get('/detailsActu.html/{id_summary}/{id_news}',[Controller::class,'Details'])->name('detailsActu.html');
+
+Route::get('/UpdateActu.html/{id_summary}/{id_news}',[Controller::class,'Update'])->name('UpdateActu.html');
+
+Route::post('/traitementUpdate',[Controller::class,'TraitementUpdate'])->name('traitementUpdate') ;
+
+Route::get('/DeleteActu.html/{id_news}',[Controller::class,'Delete'])->name('DeleteActu.html');
+
+Route::get('/frontOffice.html', [Controller::class,'ListeActualiteFront']) ;
+
+Route::get('/detailsFrontOffice.html/{summary_news}/{id_news}', [Controller::class,'DetailsFrontOffice']) ;
+
+Route::post('/insererCitation',[Controller::class,'InsererCitation'])->name('insererCitation') ;
+
